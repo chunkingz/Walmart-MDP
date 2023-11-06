a93be0e05de7c1013b72751540c23e1e3a4b391b
chunkingz <fortuneking33@yahoo.com>
2023-11-06 01:59:56 -0600

    first commit

---
+++ app/src/main/java/com/chunkingz/walmartlogin/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.chunkingz.walmartlogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chunkingz.walmartlogin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
